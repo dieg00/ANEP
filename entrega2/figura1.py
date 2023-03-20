d032590d4f3cb57863943d0724e5272fd96051a9
dieg00 <diegogonvillar2@gmail.com>
2023-03-20 11:57:31 +0100

    Figura1 is completed

--- entrega2/figura1.py
+++ entrega2/figura1.py
@@ -7,6 +7,13 @@ def M(x):
     else:
         return 1/30+np.pi/6
 Y=[M(x) for x in X]
+Y_pi = np.linspace(np.min(Y)+0.05,M(np.pi),1000)
+X_pi = np.pi*np.ones_like(Y_pi)
 plt.plot(X,Y)
-plt.scatter(np.pi,M(np.pi))
+plt.scatter(np.pi,M(np.pi),c='black')
+plt.xlabel("Tiempo t")
+plt.ylabel("Masa total M(t)")
+plt.title("Evolución masa total")
+plt.plot(X_pi, Y_pi, linestyle='dashed', c='black')
+plt.text(np.pi, np.min(Y), "t=π", ha='center', va='bottom', fontsize=12)
 plt.show()
