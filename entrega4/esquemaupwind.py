1e7aee65b3cc346b49cfde7ffbd9307852b02788
dieg00 <diegogonvillar2@gmail.com>
2023-05-01 19:17:35 +0200

    estamos en el final de la primera mitad de anep 4

--- entrega4/esquemaupwind.py
+++ entrega4/esquemaupwind.py
@@ -1,2 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def crear_matriz(N,M):
+    return np.zeros([M+1,N+1])
+
+def vs(x):
+    if x<=50:
+        return 70
+    elif x>=60:
+        return 120
+    else:
+        return 70+50*(1-(60-x)/10)
+
+#def theta_value(matriz_theta,n,m,parametro_lambda,x0=0,delta_x=0.01,t0=0,delta_t=0.01):
+#    matriz_theta[n,m] = matriz_theta[n-1,m]-vs(x0+n*delta_t)*parametro_lambda*(matriz_theta[n-1,m]-matriz_theta[n-1,m-1])
+#    return matriz_theta
+
+def condis_iniciales_x(x):
+    return vs(x)*np.exp(-(np.abs(x-10)**2)/25)
+
+def condis_iniciales_t(t):
+    return 7
+
+def representar_mallado(mallado,title,xlabel='Eje x',ylabel='Eje t',N=1000,M=1000, plot_show=False, x_inicial=0, x_final=100, t_inicial=0, t_final=1):
+    # Funcion que genera un mapa de calor para una matriz dada. Permite seleccionar el titulo, el nombre de cada eje, los valores relativos al número de puntos y extremos (para poder marcar correctamente los ejes), y tiene un toggle plot_show por si queremos que se ejecute plt.show() al acabar, cosa que no querremos al buscar que se formen varios plots a la vez. La función está extraída con ligeras modificaciones de la entrega anterior.
+    fig, ax = plt.subplots()
+    im = ax.imshow(mallado, aspect='auto', cmap='hot')
+    cbar = ax.figure.colorbar(im, ax=ax)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    y_ticks = np.linspace(t_inicial, t_final, 6)
+    ax.set_yticks(np.linspace(0, M, 6))
+    ax.set_yticklabels(np.round(y_ticks, decimals=1))
+    ax.invert_yaxis()
+    x_ticks = np.linspace(x_inicial, x_final, 6)
+    ax.set_xticks(np.linspace(0, N, 6))
+    ax.set_xticklabels(np.round(x_ticks, decimals=0))
+    if plot_show:
+        plt.show()
+
+def calcular_theta(N,M,x0=0,xf=100,t0=0,tf=1):
+    matriz_theta = crear_matriz(N,M)
+    delta_x = (xf-x0)/N
+    delta_t = (tf-t0)/M
+    parametro_lambda = delta_t/delta_x
+    for n in range(N+1):
+        matriz_theta[0,n] = condis_iniciales_x(x0+delta_x*n)
+    for m in range(M+1):
+        matriz_theta[m,0] = condis_iniciales_t(t0+delta_t*m)
+    for n in range(1,N+1):
+        for m in range(1,M+1):
+            matriz_theta[m,n] = matriz_theta[m-1,n]-vs(x0+delta_x*n)*parametro_lambda*(matriz_theta[m-1,n]-matriz_theta[m-1,n-1])
+    representar_mallado(matriz_theta, N=N, M=M, title="θ(x,t) Modelo Simplificado", plot_show=True)
+    return matriz_theta
+
+b = calcular_theta(1000,1000)
